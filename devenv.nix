4b9c033424bcf2fab0b58c76f6eed4e614212b26
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-14 10:56:05 +1000

    chore: exclude devcontainer.json from prettier and add prek to cspell

--- devenv.nix
+++ devenv.nix
@@ -180,6 +180,7 @@ in
         enable = true;
         excludes = [
           "module/README.md"
+          "\\.devcontainer/devcontainer\\.json$"
         ];
       };
       # Use prettier instead.
